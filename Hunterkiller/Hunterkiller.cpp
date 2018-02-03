786e579c17c18b94333fcaf553f7540400720c60
DUSTIN BINGHAM <DUSBINGH@uat.edu>
2018-02-03 16:05:13 -0700

    HunterKiller1

--- Hunterkiller/Hunterkiller.cpp
+++ Hunterkiller/Hunterkiller.cpp
@@ -2,10 +2,50 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <ctime>
+using std::cout;
+using std::endl;
 
 
 int main()
 {
-    return 0;
+
+	cout << "Generate Random enemy location on 8x8 grid...." << endl;
+	srand(time(0));
+
+
+	int searchGridHighNumber = 64;
+	int searchGridLowNumber = 1;
+	int enemyLocation = rand() % searchGridHighNumber + searchGridLowNumber;
+	int targetLocationPredictionCounter = 0;
+	bool targetFound = false;
+
+	cout << "Begin search." << endl;
+
+	while (targetFound == false)
+	{
+		int targetLocationPrediction = ((searchGridHighNumber - searchGridLowNumber) / 2) + searchGridLowNumber;
+
+		targetLocationPredictionCounter++;
+
+		if (targetLocationPrediction > enemyLocation)
+		{
+			searchGridHighNumber = targetLocationPrediction - 1;
+			cout << "Too high." << endl;
+
+		}
+		else if (targetLocationPrediction < enemyLocation)
+		{
+			searchGridLowNumber = targetLocationPrediction + 1;
+			cout << "Too low." << endl;
+		}
+		else
+		{
+			cout << "Hunted and Killed after " << targetLocationPredictionCounter << " areas searched." << endl;
+			targetFound = true;
+		}
+	}
+	return 0;
 }
 
